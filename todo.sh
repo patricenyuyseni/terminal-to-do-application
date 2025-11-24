2bfbd618149d931a016f7c88a024e05458e2ecfe
Patrice Nyuyseni <patriceflezy123@gmail.com>
2025-11-24 09:26:16 +0100

    changes made on tasks file

--- todo.sh
+++ todo.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+TASK_FILE="task.txt"
+touch "$TASK_FILE"
+case "$1" in 
+add)
+
+if [ -z "$2" ]; then
+echo "no task"
+exit 1
+fi
+
+# APPEND TASK
+echo "$2" >> "$TASK_FILE" # "$2" is the second argument
+echo "tasks added: $2"
+;;
+*)
+esac
+
